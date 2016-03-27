57db8fbf8151f4d17426e95119992e2e317d6a89
rainmote <rainmote@gmail.com>
2016-03-27 23:33:02 +0800

    first

---
+++ progress_bar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RUN_TIMES=$1
+FAIL_COUNT=0
+DISPLAY_COUNT=50
+
+run_rate=$(printf  "%.2f" `echo "scale=2;100/$RUN_TIMES"|bc`)
+display_rate=$(printf "%.2f" `echo "scale=2;$RUN_TIMES/100/$DISPLAY_COUNT"|bc`)
+
+for i in `seq 0 $run_rate 100`
+do
+    b=''; for i in `seq $i`; do b+='#'; done
+    printf "progress: [%-100s] %d%%  %d\r" $b $i $FAIL_COUNT
+
+    sleep 0.1
+
+    if [ $? -ne 0 ]; then
+        FAIL_COUNT+=1
+    fi
+done
+echo
